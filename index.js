a4a7c3b5d90fc95c02591b7c9691d5fcd72208b6
Hansel-Christopher <chris23han@gmail.com>
2022-08-12 11:36:33 +0530

    Refactor

--- index.js
+++ index.js
@@ -5,55 +5,60 @@ const net = require('net');
 const sendAlert = require('./sendAlert');
 
 app.use(express.json()) 
+const hostname = "localhost"
 const port = 8000;
 
 const run = function(healthCheckOptions) {
     
     const socket = net.connect(healthCheckOptions.port, healthCheckOptions.hostname);
 
-    let response = {};
+    return new Promise(resolve => {
+        let response = {};
+        const timer = setTimeout(() => {
+            socket.destroy();
+            response.ok = false;
+            response.result = '[503] Gateway error';
+            response.checkedAt = new Date();
+            console.error(`Health check "${healthCheckOptions.name}" failed: ${response.result}`);    
+            resolve(response)
+        }, healthCheckOptions.timeout);
 
-    const timer = setTimeout(() => {
-        socket.destroy();
-        response.ok = false;
-        response.result = '[503] Gateway error';
-        response.checkedAt = new Date();
-        console.error(`Health check "${healthCheckOptions.name}" failed: ${response.result}`);    
-    }, healthCheckOptions.timeout);
 
+        socket.on('connect', () => {
+            clearTimeout(timer);
+            socket.destroy();
+            response.ok = true;
+            response.result = '[200] OK';
+            response.checkedAt = new Date();
+            console.log(`Health check "${healthCheckOptions.name}" succeeded: ${response.result}`)        
+            resolve(response)
+        });
 
-    socket.on('connect', () => {
-        clearTimeout(timer);
-        socket.destroy();
-        response.ok = true;
-        response.result = '[200] OK';
-        response.checkedAt = new Date();
-        console.log(`Health check "${healthCheckOptions.name}" succeeded: ${response.result}`)        
+        socket.on('error', error => {
+            clearTimeout(timer);
+            socket.destroy();
+            response.ok = false;
+            response.result = error.message;
+            response.checkedAt = new Date();
+            console.error(`Health check "${healthCheckOptions.name}" failed: ${error.message}`);    
+            resolve(response)
+        });
     });
-
-    socket.on('error', error => {
-        clearTimeout(timer);
-        socket.destroy();
-        response.ok = false;
-        response.result = error.message;
-        response.checkedAt = new Date();
-        console.error(`Health check "${healthCheckOptions.name}" failed: ${error.message}`);        
-    });
-    
-    return response;
 };
 
 const requestListener = function (req, res) {
     const checkOptions = req.body;
-    response = run(checkOptions)
-    
-    if (!response.ok){
-        sendAlert.alert(response, checkOptions)
-    }    
+    run(checkOptions).then((response) => {
+        if (!response.ok){
+            sendAlert.alert(response, checkOptions)
+        }    
+    });    
 
     res.send("Health checks completed!");
 };
 
-app.listen(port)
+app.listen(port, hostname, () => {
+    console.log(`Health check service running on http://${hostname}:${port}`);
+})
 
 app.post('/healthCheck', requestListener)
--- sendAlert.js
+++ sendAlert.js
@@ -6,7 +6,7 @@ publishToSns = function(response, checkOption) {
 
     let status = response.ok ? "HEALTHY" : "UNHEALTHY"
     var params = {
-        Message: `Check: ${checkOption.name}; Endpoint: ${checkOption.hostname}; Status: ${status}; BusinessImpact: ${checkOption.businessImpact}`,
+        Message: `Check: ${checkOption.name}; Endpoint: ${checkOption.hostname}; Status: ${status}; BusinessImpact: ${checkOption.businessImpact}; CheckedAt: ${response.checkedAt}`,
         TopicArn: 'arn:aws:sns:us-west-2:609850474485:sample-alerting'
     };
 
